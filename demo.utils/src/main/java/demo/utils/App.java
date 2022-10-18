170169859391f6897afc9e21f1c7c6095137bd31
Trisna Nadi Selamet <Trisna.Selamet@SINARMASMSIGLIFE.CO.ID>
2022-10-18 22:57:53 +0700

    static ,void dan return

--- demo.utils/src/main/java/demo/utils/App.java
+++ demo.utils/src/main/java/demo/utils/App.java
@@ -1,13 +1,32 @@
 package demo.utils;
 
 public class App {
-	static final String data = "Bill"; //object static final
+	
+	static final String wa = "Billi"; //object static final
+	static final int karyawan = 5;
+	String nama ,umur; // variable local
+	
+	String department = "IT";
+	String jabatan = "Head IT";
 	
 	
 	public static void main (String [] args) { //static void main
+		
+		
+		App getData = new App();// Membuat Objek dari Class App
+		getData.getIdentitas(); //memangambil objek dari void getIdentitas
+		
+		
+		System.out.println("Departemen = "+getData.getDepartment());//mengembalikan objek dari return getDepartment
+		System.out.println("Jabatan = "+getData.getJabatan()); //mengembalikan objek dari return getJabatan
+		
+		
 		while(true) {
+			
+			if(wa!=null || wa.equals(wa)) {
+				
 			try {
-				getData(data );
+				getData(wa);
 				Integer time = checkData();
 				Thread.sleep(time);
 			} catch (Exception e) {
@@ -15,21 +34,49 @@ public class App {
 			 System.out.print(e.toString());
 			}	
 		}
+			System.out.println("Pesan selesai dikirim ");
+			break;
+	 }
+		
 	}
 
 	
 	public static Integer checkData() { //return with parameter
-		Integer time = 60000;
-		//time;
+		Integer time = 60000;//time
 		return time;
 	}
 	public static void getData(String check) { //void with parameter
-		//check ;
-		if(check.equals("Billi")) {
-			System.out.println("Data sama = "+ check);
-		}else {
-			System.out.println("Data beda = "+check);
-		}
 		
+		for(int i = 1; i <= karyawan; i++) {
+		if(check.equals("Billi")) //check	
+			try {
+				Thread.sleep(10000);
+				System.out.println("Pesan dikirim ke karyawan "+ i +" = pesan dari "+ check);
+				//Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.print(e.toString());
+			}
+		else {
+			System.out.println("Pesan tidak dikirim ke karyawan");
+			break;
+		  }
+		}
 	}
+	
+	void getIdentitas() { //void tanpa parameter
+		nama = "Billi";
+		umur = "17";
+		System.out.println("Nama saya = "+ nama);
+		System.out.println("Umur saya = "+ umur);
+	}
+	
+	String getDepartment() { //return tanpa parameter
+		return department;
+	}
+	String getJabatan() { //return tanpa parameter
+		return jabatan;
+	}
+	
 }
